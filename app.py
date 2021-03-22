e581b878ca217a8a00416fa499d6349b8e090812
Satvik Bansal <54990847+Satvik-B@users.noreply.github.com>
2021-03-23 05:19:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for 
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/adminLogin')
+def adminLogin():
+    return render_template('adminLogin.html')
+
+@app.route('/customerSignup')
+def customerSignup():
+    return render_template('customerSignup.html')
+
+@app.route('/restaurantSignup')
+def restaurantSignup():
+    return render_template('restaurantSignup.html')
+
+@app.route('/deliveryAgentSignup')
+def deliveryAgentSignup():
+    return render_template('deliveryAgentSignup.html')
+
+if __name__ == "__main__":
+    # cache.init_app(app)
+    app.run(debug=True)
