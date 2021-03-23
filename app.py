fc3cbaa81d9a4bb92ea33ace8a8fd5ed3f570c6c
anishsofat <sofatanish16@gmail.com>
2021-03-24 01:02:15 +0530

    Integrated Firebase to app.py

--- app.py
+++ app.py
@@ -1,8 +1,63 @@
-from flask import Flask, render_template, url_for 
+import firebase_admin
+import pyrebase
+from firebase_admin import credentials, auth
+import json
+from flask import Flask, render_template, url_for, request 
+from functools import wraps
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD']=True
+
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbConfig.json')))
+
+JWT_GLOBAL =""
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            user = auth.verify_id_token(request.headers['authorization'])
+            request.user = user
+        except:
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
+
+@app.route('/api/userinfo')
+@check_token
+def userinfo():
+    return {'data': users}, 200
+
+@app.route('/api/signup')
+def signup():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'},400
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'},200
+    except:
+        return {'message': 'Error creating user'},400
+
+@app.route('/api/token')
+def token():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        JWT_GLOBAL =jwt = user['idToken']
+        return {'token': jwt}, 200
+    except:
+        return {'message': 'There was an error logging in'},400
 
 @app.route('/')
 def index():
