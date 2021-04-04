1a9ec7d634ed5133f4c58a40b1873bb45b71aee1
anishsofat <sofatanish16@gmail.com>
2021-04-05 02:27:38 +0530

    rAN DOM

--- app.py
+++ app.py
@@ -444,22 +444,22 @@ def foodItemAdder():
             "pricePerItem" : price,
             "isRecommended": False,
             "ratingId": "",
-            "restaurantId" : session["user_id"],
+            "restaurantId" : session["userId"],
             "picSrc": ""
         }
-        doc_reference = db.collection("restaurant").document(session["user_id"]).collection("foodItem").document()
+        doc_reference = db.collection("restaurant").document(session["userId"]).collection("foodItem").document()
         doc_reference.set(foodItem)
-        doc_reference1 = db.collection("restaurant").document(session["user_id"]).collection("foodItem").document(doc_reference.id).update({"foodItemId":doc_reference.id})
+        # doc_reference1 = db.collection("restaurant").document(session["userId"]).collection("foodItem").document(doc_reference.id).update({"foodItemId":doc_reference.id})
         # return {"ok":"True"},200
         
     except:
         session['food_item_addition_msg'] = "Error adding food item text data in database"
         return redirect(url_for('addFoodItem'))
     try:
-        storage_file_path = "restaurant/"+session["user_id"]+"_"+doc_reference.id+".jpg"
+        storage_file_path = "restaurant/"+session["userId"]+"_"+doc_reference.id+".jpg"
         blob = bucket.blob(storage_file_path)
         blob.upload_from_file(local_file_obj,content_type="image/jpeg")
-        doc_reference = db.collection("restaurant").document(session["user_id"]).collection("foodItem").document(doc_reference.id).update({"picSrc":storage_file_path})
+        doc_reference = db.collection("restaurant").document(session["userId"]).collection("foodItem").document(doc_reference.id).update({"picSrc":storage_file_path})
         session['food_item_addition_msg']="Food item text and photo successfully added in database."
         return redirect(url_for('createMenu'))
     except Exception as e:
@@ -672,6 +672,7 @@ def placeOrder():
     doc_reference.set(currentOrder)
     db.collection('order').document(doc_reference.id).update({ 'orderId' : doc_reference.id})
     orderId=doc_reference.id
+    session['currentOrderCreating']["orderId"] = orderId
     # add restaurant array
     restaurantId=currentOrder['restaurantId']
     restaurantDocReference = db.collection('restaurant').document(restaurantId)
@@ -1312,13 +1313,16 @@ def ratingCustomer():
         return redirect(url_for('logout'))
 
 
-    deliveryAgentId=session['currentOrderCreating']['deliveryAgentId']
+    orderId=session['currentOrderCreating']['orderId']
+    deliveryAgentId = db.collection("order").document(orderId).get().to_dict()['deliveryAgentId']
     deliveryAgentRating=request.form['deliveryAgentRating']
     deliveryAgentRating=int(deliveryAgentRating)
 
     deliveryAgentRatingId=db.collection('deliveryAgent').document(deliveryAgentId).get().to_dict()['ratingId']
     deliveryAgentRatingObject=db.collection('rating').document(deliveryAgentRatingId).get().to_dict()
-
+    
+    
+    
     deliveryAgentRatingObject['noOfInputs'] = deliveryAgentRatingObject['noOfInputs'] + 1
     deliveryAgentRatingObject['sum'] = deliveryAgentRatingObject['sum'] + deliveryAgentRating
     deliveryAgentRatingObject['rating'] = deliveryAgentRatingObject['sum']/deliveryAgentRatingObject['noOfInputs']
@@ -1326,7 +1330,8 @@ def ratingCustomer():
     db.collection('rating').document(deliveryAgentRatingId).set(deliveryAgentRatingObject)
 
 
-    restaurantId=session['currentOrderCreating']['restaurantId']
+    restaurantId = db.collection("order").document(orderId).get().to_dict()['restaurantId']
+    # restaurantId=session['currentOrderCreating']['restaurantId']
     restaurantRating=request.form['restaurantRating']
     restaurantRating=int(restaurantRating)
 
